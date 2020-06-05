e28cd94f33365ff294473b5bf2075786480f2bc0
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2020-06-05 16:36:57 -0300

    implementando carregamento de fotos do intagram via javascript

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,14 +10,14 @@ class ProfilesController < ApplicationController
     Statistic.track!(@profile, Statistic.events[:perfil_view]) unless current_user? @profile.user
 
     # instagram scraping
-    if @profile.instagram_account.present?
-      @instagram_scrap = scrap_from_instagram(@profile.instagram_account.username)
+    # if @profile.instagram_account.present?
+      # @instagram_scrap = scrap_from_instagram(@profile.instagram_account.username)
       # override avatar_url on profile
-      if @instagram_scrap[:avatar_url] && (@instagram_scrap[:avatar_url] != @profile.avatar_url)
-        @profile.avatar_url = @instagram_scrap[:avatar_url]
-        @profile.save(touch: false)
-      end
-    end
+      # if @instagram_scrap[:avatar_url] && (@instagram_scrap[:avatar_url] != @profile.avatar_url)
+      #   @profile.avatar_url = @instagram_scrap[:avatar_url]
+      #   @profile.save(touch: false)
+      # end
+    # end
   end
 
   # GET /buscar?q=
@@ -100,21 +100,21 @@ class ProfilesController < ApplicationController
   #   render "search"
   # end
 
-  private
-    def scrap_from_instagram(username)
-      instagram_scrap = {}
-      response = HTTParty.get("https://www.instagram.com/#{username}/?__a=1")
-      logger.debug "[PROFILE::INSTAGRAM SCRAPPING] Response: \n#{response}"
-      if response["graphql"].present?
-        instagram_scrap[:avatar_url] = response["graphql"]["user"]["profile_pic_url"]
-        instagram_scrap[:avatar_url_large] = response["graphql"]["user"]["profile_pic_url_hd"]
-        if response["graphql"]["user"]["edge_owner_to_timeline_media"]["count"] > 0
-          instagram_scrap[:pictures] = []
-          response["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"].each do |post|
-            instagram_scrap[:pictures].push({original: post["node"]["thumbnail_src"], thumbnail: post["node"]["thumbnail_resources"].first["src"]})
-          end
-        end
-      end
-      return instagram_scrap
-    end
+  # private
+  #   def scrap_from_instagram(username)
+  #     instagram_scrap = {}
+  #     response = HTTParty.get("https://www.instagram.com/#{username}/?__a=1")
+  #     logger.debug "[PROFILE::INSTAGRAM SCRAPPING] Response: \n#{response}"
+  #     if response["graphql"].present?
+  #       instagram_scrap[:avatar_url] = response["graphql"]["user"]["profile_pic_url"]
+  #       instagram_scrap[:avatar_url_large] = response["graphql"]["user"]["profile_pic_url_hd"]
+  #       if response["graphql"]["user"]["edge_owner_to_timeline_media"]["count"] > 0
+  #         instagram_scrap[:pictures] = []
+  #         response["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"].each do |post|
+  #           instagram_scrap[:pictures].push({original: post["node"]["thumbnail_src"], thumbnail: post["node"]["thumbnail_resources"].first["src"]})
+  #         end
+  #       end
+  #     end
+  #     return instagram_scrap
+  #   end
 end
