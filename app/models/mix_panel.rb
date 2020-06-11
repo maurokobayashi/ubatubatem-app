c5fec68a6a97ae0311e4c2c383e1823420789b3b
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2020-06-11 13:27:18 -0300

    implementando todos os analytics do Profile page

--- app/models/mix_panel.rb
+++ app/models/mix_panel.rb
@@ -1,6 +1,33 @@
 class MixPanel
 
+  # Catalogo events
+  CATALOGO_VIEW = "visit.catalogo"
+  CATALOGO_CATEGORIA_VIEW = "visit.catalogo_categoria"
+  CATALOGO_BAIRRO_VIEW = "visit.catalogo_bairro"
+
+  # Profile events
   PROFILE_VIEW = "visit.profile"
+  PROFILE_EDIT_VIEW = "visit.profile_edit"
+  PROFILE_ADDRESS_CLICK = "click.profile_address"
+  PROFILE_AVATAR_CLICK = "click.profile_avatar"
+  PROFILE_BOOKMARK_CLICK = "click.profile_bookmark"
+  PROFILE_INSTAGRAM_CLICK = "click.profile_instagram"
+  PROFILE_OPENING_HOURS_CLICK = "click.profile_opening_hours"
+  PROFILE_PHONE_CLICK = "click.profile_phone"
+  PROFILE_PHOTO_CLICK = "click.profile_photo"
+  PROFILE_STATISTICS_CLICK = "click.profile_statistics"
+  PROFILE_WHATSAPP_CLICK = "click.profile_whatsapp"
+
+  # Search events
   SEARCH_SUBMIT = "submit.search"
+  SEARCH_RESULTS_VIEW = "visit.search_results"
+
+  # Share events
+  SHARE_CLICK = "click.share"
+
+  # Authentication events
+  SIGNIN_VIEW = "visit.signin"
+  SIGNIN_SUBMIT = "submit.signin"
+  SIGNOUT_SUBMIT = "submit.signout"
 
 end
