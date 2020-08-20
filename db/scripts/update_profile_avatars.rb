6729ab0b1cb300d77bbe3d2112e061a9f455fd72
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2020-08-20 18:17:47 -0300

    script zica para atualizar os avatares

--- db/scripts/update_profile_avatars.rb
+++ db/scripts/update_profile_avatars.rb
@@ -3,20 +3,18 @@
 
 # ATENÇÃO! ESSE SCRIPT DEVE SER EXECUTADO EM LOCALHOST, POIS O INSTAGRAM BLOQUEOU REQUISIÇÕES A PARTIR DO SERVIDOR DO HEROKU
 
-profile_ids = HTTParty.get("https://www.ubatubatem.app/profiles.json")
-profile_ids.each do |id|
-  profile = Profile.find(id)
+profiles_response = HTTParty.get("https://www.ubatubatem.app/profiles.json?limit=900&offset=110")
+profiles = JSON(profiles_response.body)
+profiles.each do |p|
+  puts "Fazendo SCRAPPING do perfil #{p['username']} no Instagram..."
+  instagram_response = HTTParty.get("https://www.instagram.com/#{p['username']}/?__a=1")
+  avatar_url = instagram_response["graphql"].present? ? instagram_response["graphql"]["user"]["profile_pic_url"] : nil
 
-  response = HTTParty.get("https://www.instagram.com/#{lead_insta.username}/?__a=1")
+  puts "Atualizando avatar do perfil ID #{p['id']} no ubatubatem.app..."
+  data = {profile: {avatar_url: avatar_url}}
+  update_response = HTTParty.patch("https://www.ubatubatem.app/profiles/#{p['id']}/avatar", :body => data)
+  puts "HTTP Status: #{update_response.code}\n\n"
 end
 
-LeadInstagram.where("id >= 700").each do |lead_insta|
-  if !InstagramAccount.exists?(username: lead_insta.username)
-    response = HTTParty.get("https://www.instagram.com/#{lead_insta.username}/?__a=1")
-    bio = response["graphql"].present? ? response["graphql"]["user"]["biography"] : nil
-    title = lead_insta.title.present? ? lead_insta.title : lead_insta.username
-    profile = Profile.create(username: lead_insta.username, avatar_url: lead_insta.avatar_url, title: title, tagline: title, bio: bio, status: 0)
-    puts profile.inspect
-    InstagramAccount.create(profile: profile, username: lead_insta.username, instagram_user_id: lead_insta.instagram_user_id)
-  end
-end
+
+
