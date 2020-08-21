2c4ba6a7dfdf3247b17c225c38c32154aaaa1094
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2020-08-21 09:23:45 -0300

    bugfix: script de criacao de profiles a partir de leads

--- db/scripts/create_profile_from_leads.rb
+++ db/scripts/create_profile_from_leads.rb
@@ -6,7 +6,7 @@ LeadInstagram.where("id > 927").each do |lead_insta|
     response = HTTParty.get("https://www.instagram.com/#{lead_insta.username}/?__a=1")
     # bio = response["graphql"].present? ? response["graphql"]["user"]["biography"] : nil
     title = lead_insta.title.present? ? lead_insta.title : lead_insta.username
-    profile = Profile.create(username: lead_insta.username, avatar_url: lead_insta.avatar_url, title: title, tagline: title, bio: bio, status: 0)
+    profile = Profile.create(username: lead_insta.username, avatar_url: lead_insta.avatar_url, title: title, tagline: title, status: 0)
     puts profile.inspect
     InstagramAccount.create(profile: profile, username: lead_insta.username, instagram_user_id: lead_insta.instagram_user_id)
   end
